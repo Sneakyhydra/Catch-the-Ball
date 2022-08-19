0d82186955d8be74b60d723076bad89e5c4124c2
Sneakyhydra <dhruvrishishwar2@gmail.com>
2022-08-19 22:56:29 +0530

    Basic Setup

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import './App.css';
+
+const App = () => {
+	return <h1>Drag And Drop</h1>;
+};
+
+export default App;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+/**
+ * Database
+ */
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json({ extended: false }));
+app.use(cors());
+app.use(cookieParser());
+
+app.use('/api/auth', require('./routes/auth'));
+
+/**
+ * Serve static assets in production
+ */
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
